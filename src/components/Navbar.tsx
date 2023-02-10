47a02317a5174d247f42999d786e55333e5e277f
Vinicius Campos Goulart <61381596+vinicgoulart@users.noreply.github.com>
2023-02-10 15:01:03 -0300

    first commit, navbar v1 finished

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,16 @@
+function Navbar(){
+    return(
+        <div className="navbar">
+            <div className="LogoDiv">
+                <p className="logoImg"><span>Pay</span>me!</p>
+            </div>
+
+            <div className="actionDiv">
+                <p className="Loginbtn">Login</p>
+                <p className="Registerbtn">Register</p>
+            </div>
+        </div>
+    );
+}
+
+export default Navbar;
---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,15 @@
+import Navbar from "../components/Navbar";
+
+function LandingPage(){
+    return(
+        <>
+            <Navbar />
+            <div className="App">
+                <p>Hello world!</p>
+            </div>
+        </>
+    );
+}
+
+
+export default LandingPage;
