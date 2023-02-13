ac2a76b0efa5e22b25fdac279d2afac35cfc3daa
Vinicius Campos Goulart <61381596+vinicgoulart@users.noreply.github.com>
2023-02-13 14:30:42 -0300

    Landing Page frontend finished, footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,15 @@
+function Footer(){
+    return(
+        <footer>
+            <div className="footerLogo">
+                <p>Payme!</p>
+            </div>
+            <div className="terms">
+                <p>Privacy terms</p>
+                <p>Input tips!</p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,12 +1,24 @@
 import Navbar from "../components/Navbar";
+import Infoimage from '../assets/infoimage.svg';
+import Footer from "../components/Footer";
 
 function LandingPage(){
     return(
         <>
             <Navbar />
             <div className="App">
-                <p>Hello world!</p>
+                <div className="appInfo">
+                    <div className="infoText">
+                        <p>Manage your payments using <a>Pay</a><span>me!</span> Check if you need to pay someone or if someone needs to pay you!</p>
+                        <p className="infoSignup">Start now!</p>
+                    </div>
+
+                    <div className="infoImg">
+                        <img src={ Infoimage } />
+                    </div>
+                </div>
             </div>
+            <Footer />
         </>
     );
 }
